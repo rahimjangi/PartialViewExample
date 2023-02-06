41d03859c3486d135022917b99c78abd67963f8c
pc <rahimjangi1982@gmail.com>
2023-02-06 13:29:08 +0100

    Add project files.

---
+++ PartialViewExample/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PartialViewExample.Controllers;
+
+public class HomeController : Controller
+{
+    [Route("/")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [Route("About")]
+    public IActionResult About()
+    {
+        return View();
+    }
+}
